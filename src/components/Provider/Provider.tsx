c0edb29b9b4e61e5c7374cfabf8c667f0a080e92
Luis Hernandez <luauhern@gmail.com>
2022-05-24 19:31:00 -0400

    feat: add provider deletion bulk deletion
    I went back and forth on whether the ProviderList <ul/> should be wrapped
    in a <form/> element and the remove button should be used as form submission due to the Provider item having a selection <input/>. Ultimately I decided against that because my intuition told me that this use case is not a "true" form submission.

--- src/components/Provider/Provider.tsx
+++ src/components/Provider/Provider.tsx
@@ -1,3 +1,5 @@
+import React, { Dispatch, SetStateAction } from "react";
+
 export interface Provider {
   last_name: string;
   first_name: string;
@@ -8,17 +10,38 @@ export interface Provider {
 
 interface ProviderItemProps {
   provider: Provider;
+  setDeleteQueue: Dispatch<SetStateAction<string[]>>;
 }
 
-const ProviderItem = ({ provider }: ProviderItemProps) => (
-  <li key={provider.email_address} className="list__item">
-    <div className="provider">
-      <h2 className="provider__name">{`${provider.last_name}, ${provider.first_name}`}</h2>
-      <p className="provider__specialty">{provider.specialty}</p>
-      <p className="provider__email">{provider.email_address}</p>
-      <p className="provider__practice">{provider.practice_name}</p>
-    </div>
-  </li>
-);
+const ProviderItem = ({ provider, setDeleteQueue }: ProviderItemProps) => {
+  const toggleCheckbox = (e: React.FormEvent<HTMLInputElement>) => {
+    // if the click checks a provider's checkbox, lets add them to the deletion queue for a bulk delete. Otherwise, let's do remove them from the deletion queue.
+    const providerClicked = e.currentTarget;
+    if (providerClicked.checked) {
+      setDeleteQueue((prevState) => [...prevState, providerClicked.value]);
+      return;
+    }
+
+    setDeleteQueue((prevState) =>
+      prevState.filter((provider) => provider !== providerClicked.value)
+    );
+  };
+
+  return (
+    <li key={provider.email_address} className="list__item">
+      <input
+        type="checkbox"
+        value={provider.email_address}
+        onClick={toggleCheckbox}
+      />
+      <div className="provider">
+        <h2 className="provider__name">{`${provider.last_name}, ${provider.first_name}`}</h2>
+        <p className="provider__specialty">{provider.specialty}</p>
+        <p className="provider__email">{provider.email_address}</p>
+        <p className="provider__practice">{provider.practice_name}</p>
+      </div>
+    </li>
+  );
+};
 
 export default ProviderItem;
--- src/components/ProviderList/ProviderList.tsx
+++ src/components/ProviderList/ProviderList.tsx
@@ -1,15 +1,37 @@
+import { Dispatch, SetStateAction, useState } from "react";
 import ProviderItem, { Provider } from "../Provider/Provider";
 
 export interface ProviderLsitProps {
   providers: Provider[];
+  setProviders: Dispatch<SetStateAction<Provider[]>>;
 }
-const ProviderList = ({ providers }: ProviderLsitProps) => {
+const ProviderList = ({ providers, setProviders }: ProviderLsitProps) => {
+  const [deleteQueue, setDeleteQueue] = useState<string[] | []>([]);
+
+  const handleDelete = () => {
+    // Why a use Set data structure? Two reasons: 1) Even though IE is being phased out Array.includes is not supported where Set.has is. 2) more importantly, when coming dealing with large quantities of data, has() is significanlty faster than includes()
+    const deletionQueueSet = new Set(deleteQueue);
+
+    setProviders((previousProviders) =>
+      previousProviders.filter(
+        ({ email_address }) => !deletionQueueSet.has(email_address)
+      )
+    );
+  };
   return (
-    <ul className="list">
-      {providers.map((provider) => (
-        <ProviderItem key={provider.email_address} provider={provider} />
-      ))}
-    </ul>
+    <>
+      <ul className="list">
+        {providers.map((provider) => (
+          <ProviderItem
+            {...{ provider, setDeleteQueue }}
+            key={provider.email_address}
+          />
+        ))}
+      </ul>
+      <button className="delete" onClick={handleDelete}>
+        Remove
+      </button>
+    </>
   );
 };
 
